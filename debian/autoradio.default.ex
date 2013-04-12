200c3ff68f33afd99b9a0e78ef7d3e45c35df3ad
Paolo Patruno <pat1@users.noreply.github.com>
2013-04-12 09:39:28 +0000

    Initial commit

---
+++ debian/autoradio.default.ex
@@ -0,0 +1,10 @@
+# Defaults for dbusgpio initscript
+# sourced by /etc/init.d/dbusgpio
+# installed at /etc/default/dbusgpio by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
